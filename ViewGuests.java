c27a6473226a877d4576503ff4753b35d11f6385
Darishini <86789057+Darishini@users.noreply.github.com>
2021-07-15 22:25:49 +0800

    Update ViewGuests.java

--- ViewGuests.java
+++ ViewGuests.java
@@ -3,31 +3,27 @@ package c209_ESE;
  * I declare that this code was written by me. I will not copy or allow others
  * to copy my code. I understand that copying code is considered as plagiarism.
  *
- * 16035558, 24 June 2021 12:35:22 pm
+ * 16035558, 24 June 2021 2:29:12 pm
  */
 
 import java.util.ArrayList;
+import c209_ESE.Helper;
+import c209_ESE.Guest;
+import c209_ESE.SHNGuest;
+import c209_ESE.STCGuest;
 
 
 
 public class ViewGuests {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		ArrayList<Guest> guestList = new ArrayList<Guest>();
-
-		guestList.add(new Guest (9, "Monisha", 03-24, "STC"));
-		guestList.add(new Guest (12,"Lucifer", 02-88, "SHN"));
-		
-		
+		private ArrayList<Guest> guestList = new ArrayList<Guest>();
 		
 
-		
+		public static void main(String[] args) {
 
+			ViewGuests vg = new ViewGuests();
+			vg.start();
 		
-			Guest guest = new Guest();
-			guest.start();
 		}
 
 		private void start() {
@@ -60,7 +56,7 @@ public class ViewGuests {
 		// TODO Auto-generated method stub
 		
 		String output = "";
-		output += String.format("-20s%-10s\n", "REG ID", "NAME", "ROOM NO", "GUEST TYPE");
+		output += String.format("-20s%-10s\n", "REG ID", "NAME", "ROOM NO", "GUEST TYPE", "DAYS TO SERVE", "TRAVEL DOC");
 		for (Guest i : guestList) {
 			output += String.format("-20s%-10.2f\n", i.getID(), i.getName(), i.getRoomNo(), i.getGuestType());
 		}
@@ -71,12 +67,23 @@ public class ViewGuests {
 
 	private static void viewSTCGuest(ArrayList<Guest> guestList) {
 		// TODO Auto-generated method stub
-		
+		String output = "";
+		output += String.format("-20s%-10s\n", "REG ID", "NAME", "ROOM NO", "GUEST TYPE", "DAYS TO SERVE", "TRAVEL DOC");
+		for (Guest i : guestList) {
+			output += String.format("-20s%-10.2f\n", i.getID(), i.getName(), i.getRoomNo(), i.getGuestType());
+		}
+		System.out.println(output);
 	}
 
 
 	private static void viewAllGuest(ArrayList<Guest> guestList) {
 		// TODO Auto-generated method stub
+		String output = "";
+		output += String.format("-20s%-10s\n", "REG ID", "NAME", "ROOM NO", "GUEST TYPE", "DAYS TO SERVE", "TRAVEL DOC");
+		for (Guest i : guestList) {
+			output += String.format("-20s%-10.2f\n", i.getID(), i.getName(), i.getRoomNo(), i.getGuestType());
+		}
+		System.out.println(output);
 		
 	}
 
@@ -95,3 +102,4 @@ public class ViewGuests {
 	}
 
 }
+
