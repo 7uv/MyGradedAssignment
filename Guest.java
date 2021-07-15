fb5f99f9bc2a4e596d7b27777c60682e07a3ad19
Darishini <86789057+Darishini@users.noreply.github.com>
2021-07-15 22:22:53 +0800

    Update Guest.java

--- Guest.java
+++ Guest.java
@@ -1,32 +1,68 @@
 package c209_ESE;
+/**
+ * I declare that this code was written by me. I will not copy or allow others
+ * to copy my code. I understand that copying code is considered as plagiarism.
+ *
+ * 16035558, 24 June 2021 1:28:03 pm
+ */
 
 public abstract class Guest {
     private int id;
 	private String name;
-	private int roomNo;
+	private String roomNo;
 	private String guestType;
 	
-	
-	
+	public Guest(int id, String name, String roomNo, String guestType) {
+		this.id = id;
+		this.name = name;
+		this.roomNo = roomNo;
+		this.guestType = guestType;
 	}
+	
+	
 	
 	public int getID() {
 		return id;
 	}
 	
+	public void setID(int id) {
+		this.id = id;
+		
+	}
 	public String getName() {
 		return name;
 		
 	}
 	
-	public int getRoomNo() {
+	public void setName(String name) {
+		this.name = name;
+		
+	}
+		
+	public String getRoomNo() {
 		return roomNo;
-
+	}
+	
+		public void setRoomNo(String roomNo) {
+			this.roomNo = roomNo;
+			
 }
 	public String getGuestType() {
 		return guestType;
 
  }
 	
+	public void setGuestType(String guestType) {
+		this.guestType = guestType;
+	
+	}
+	
 	public abstract void display();
-  }
+
+
+
+	
+	
+}
+	
+	
